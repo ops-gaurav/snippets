9e91553fdd46e3a75ae3e5e53772eaa0f25c770f
Gaurav Sharma <sharma02gaurav@gmail.com>
2017-02-14 18:25:46 +0530

    commiting the new built gulp file for handling the clean and move tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 var gulp = require ('gulp');
 var sass = require ('gulp-sass');
 var minify = require ('gulp-minify');
+var clean = require ('gulp-clean');
 
 gulp.task ('test-system', () => {
     console.log ('system is up and running..');
@@ -21,4 +22,30 @@ gulp.task ('build-stylesheets', () => {
 // minify the javascript files and stylesheets and html files
 gulp.task ('minify-scripts', () => {
 
+});
+
+/**
+*	gulp task to clean up the dist folder
+*/
+gulp.task ('clean', () => {
+	gulp.src ('./dest/**/*.*')
+	.pipe (clean ({force: false}))
+	.pipe (gulp.dest ('./dist'));
+});
+
+const filesToMove = [
+	'./src/data/**/*.*',
+	'./src/img/**/*.*',
+	'./src/routers/**/*.*',
+	'./src/schemas/**/*.*',
+	'./src/views/**/*.*',
+	'./src/*.*'
+];
+
+/**
+*	gulp task to move files from src to dist directory
+*/
+gulp.task ('move', ['clean'], () =>{
+	gulp.src (filesToMove, {base: './src'})
+	.pipe (gulp.dest ('build'));
 });
