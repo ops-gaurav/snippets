4413e53ecd1b4c57f3a546cfbd1943ebc97f5b8e
Gaurav Sharma <sharma02gaurav@gmail.com>
2017-02-15 11:50:20 +0530

    form

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@ var gulp = require ('gulp');
 var sass = require ('gulp-sass');
 var minify = require ('gulp-minify');
 var clean = require ('gulp-clean');
+var uglify = require ('gulp-uglify');
 
 gulp.task ('test-system', () => {
     console.log ('system is up and running..');
@@ -14,14 +15,22 @@ gulp.task ('default', () => {
 gulp.task ('build-stylesheets', () => {
     gulp.src ('src/style/sass/**/*.scss')
         .pipe (sass().on ('error', sass.logError))
-        .pipe (gulp.dest ('build/css'));
+        .pipe (gulp.dest ('./build/css'));
 });
 
 
 //https://www.npmjs.com/package/gulp-minify
-// minify the javascript files and stylesheets and html files
+// minify the javascript files and stylesheets and html files]
+// THROWING ERROR
 gulp.task ('minify-scripts', () => {
-
+	gulp.src ('./src/**/*.js')
+		.pipe (minify ({
+			ext:{
+				src: '-debug.js',
+				min: '.js'
+			}
+		}))
+		.pipe (gulp.dest ('./build'));
 });
 
 /**
@@ -29,8 +38,8 @@ gulp.task ('minify-scripts', () => {
 */
 gulp.task ('clean', () => {
 	gulp.src ('./dest/**/*.*')
-	.pipe (clean ({force: false}))
-	.pipe (gulp.dest ('./dist'));
+	.pipe (clean ({force: true}))
+	.pipe (gulp.dest ('build'));
 });
 
 const filesToMove = [
@@ -45,7 +54,7 @@ const filesToMove = [
 /**
 *	gulp task to move files from src to dist directory
 */
-gulp.task ('move', ['clean'], () =>{
+gulp.task ('move', () =>{
 	gulp.src (filesToMove, {base: './src'})
 	.pipe (gulp.dest ('build'));
 });
